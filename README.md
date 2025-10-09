4782e135b111c96f913985b45798d66f73d33a78
Kuro <furkyuubi@gmail.com>
2025-10-09 15:45:37 +0200

    Client/Ananke: Added basic settings. README.md: Created

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Ananke.NET
+A self-hosted network management tool for controlling your own PCs on your local network.
+
+## Installation
+1) Clone the repo:
+```bash
+git clone https://github.com/Kuarma/Ananke.NET
+cd Ananke.NET
+```
+2) Restore packages:
+```bash
+dotnet restore
+```
+3) Build project:
+```bash
+dotnet build
+```
+4) Run the project:
+```bash
+dotnet run
+```
--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,3 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿#region License
 
-Console.WriteLine("Hello, World!");
+/*
+MIT License
+
+Copyright (c) 2025 Kuro
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+#endregion License
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+// Setup bootstrap logger (https://nblumhardt.com/2020/10/bootstrap-logger/)
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddSerilog((services, loggerConfiguration) =>
+        loggerConfiguration
+            .ReadFrom.Configuration(builder.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.FromLogContext());
+
+    // DPAPI  
+    builder.Services.AddDataProtection()
+        .ProtectKeysWithDpapiNG();
+
+    builder.WebHost.ConfigureKestrel(config =>
+        config.ListenLocalhost(5000));
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSerilogRequestLogging(options =>
+        {
+            options.MessageTemplate =
+                "HTTP {RequestMethod} {RequestPath} {StatusCode} {Elapsed:0.0000}ms";
+
+            options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+            {
+                diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value!);
+                diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
+            };
+        });
+    }
+
+    app.UseHttpsRedirection();
+    
+    app.UseRouting();
+    
+    app.UseAuthorization();
+
+    await app.RunAsync();
+}
+catch (Exception exception)
+{
+    Log.Fatal(exception, "Host terminated unexpectedly");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
