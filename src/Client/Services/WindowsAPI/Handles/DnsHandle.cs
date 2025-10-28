d4ad325998d6c1ee0247e0839c1a5713e04faac5
Kuro <furkyuubi@gmail.com>
2025-10-28 13:55:08 +0100

    Second refactor: There are so many flaws I've not seen yesterday. DnsHandle still needs some work.

---
+++ src/Client/Services/WindowsAPI/Handles/DnsHandle.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+using Serilog;
+
+namespace Ananke.Services.WindowsAPI.Handles;
+
+public partial class DnsHandle : SafeHandle
+{
+    [LibraryImport("dnsapi.dll")]
+    private static partial int DnsServiceRegisterCancel(
+        IntPtr pCancel);
+    
+    public DnsHandle() :
+        base(
+            invalidHandleValue: IntPtr.Zero,
+            ownsHandle: true)
+    {
+    }
+
+    public override bool IsInvalid =>
+        handle == IntPtr.Zero;
+    
+    protected override bool ReleaseHandle()
+    {
+        var ptr = Interlocked.Exchange(
+            location1: ref handle,
+            value: IntPtr.Zero);
+
+        if (ptr == IntPtr.Zero)
+            return true;
+        
+        var errorCode = DnsServiceRegisterCancel(ptr);
+        
+        if (errorCode != (int)WinSpecialCodes.ERROR_SUCCESS)
+            Log.Error(
+                messageTemplate: "Error while trying to cancel Dns service. Special code: {SpecialCode}.",
+                propertyValue: errorCode);
+
+        return true;
+    }
+}
--- src/Client/Services/WindowsAPI/WinSpecialCodes.cs
+++ src/Client/Services/WindowsAPI/WinSpecialCodes.cs
@@ -3,6 +3,11 @@
 // https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes--0-499-"
 public enum WindowsErrorCodes 
 {
+    // Success codes
     ERROR_SUCCESS = 0,
-    DNS_REQUEST_PENDING = 9506 
+    DNS_REQUEST_PENDING = 9506,
+    ERROR_CANCELLED = 1223,
+    
+    // Error codes
+    ERROR_INVALID_PARAMETER = 87,
 }
